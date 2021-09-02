6a0b909fe9c6a51fd1f7f521f2c42b314f36e98c
momokaddour <mkaddour2001@gmail.com>
2021-09-01 22:48:55 -0400

    Added addition feature

--- test.c
+++ test.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main () {
-    printf("Hello World!!!");
+    //Adding the addition feature
+    int a = 7;
+    int b = 9; 
+    int c = a + b;
+    printf("Hello World!!!\n");
+    printf("%d + %d = %d\n", a, b, c);
     return 0;
 }
